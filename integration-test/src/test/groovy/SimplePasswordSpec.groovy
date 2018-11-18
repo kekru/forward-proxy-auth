e3b59364649fdaff1931593cf82091adc58d4d35
Kevin Krummenauer <lopusknut@googlemail.com>
2018-11-18 19:59:11 +0100

    add httpclient to test

--- integration-test/src/test/groovy/SimplePasswordSpec.groovy
+++ integration-test/src/test/groovy/SimplePasswordSpec.groovy
@@ -1,17 +1,27 @@
 import spock.lang.*
-
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClientBuilder
 
 class SimplePasswordSpec extends Specification {
 
+    def client
 
-    def "abc" (){
+    def setup(){
+        client = HttpClientBuilder.create().build()
+    }
+
+    def "No token and no password are sent" (){
         given: "something"
         def a = 0
 
         when: "max is requested"
-        def max = Math.max(a, 10)
+        def res = client.execute(new HttpGet("http://localhost:8080"))
+		
 
         then: "sdgsg"
-        max == 10
+         a == 0
+        
     }
 }
